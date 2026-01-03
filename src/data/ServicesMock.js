2ff73bcf089ef0838110b88e51cb76833b8953b9
Erick6751 <erick.founders@gmail.com>
2026-01-03 18:06:49 -0500

    destination refactor 22222

---
+++ src/data/ServicesMock.js
@@ -0,0 +1,42 @@
+export const services = [
+    {
+        title: "Photo Shoot",
+        listings: 20,
+        img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=500",
+    },
+    {
+        title: "Tour Guide",
+        listings: 22,
+        img: "https://images.unsplash.com/photo-1539635278303-d4002c07eae3?q=80&w=500",
+    },
+    {
+        title: "Cozy Event",
+        listings: 23,
+        img: "https://images.unsplash.com/photo-1548574505-5e239809ee19?q=80&w=500",
+    },
+    {
+        title: "Interesting Rest",
+        listings: 24,
+        img: "https://images.unsplash.com/photo-1501785888041-af3ef285b470?q=80&w=500",
+    },
+    {
+        title: "Kayaking",
+        listings: 25,
+        img: "https://images.unsplash.com/photo-1544918877-460635b6d13e?q=80&w=500",
+    },
+    {
+        title: "Safe Flight",
+        listings: 26,
+        img: "https://images.unsplash.com/photo-1506012787146-f92b2d7d6d96?q=80&w=500",
+    },
+    {
+        title: "Entertainment",
+        listings: 27,
+        img: "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=500",
+    },
+    {
+        title: "Delicious Food",
+        listings: 28,
+        img: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?q=80&w=500",
+    },
+];
